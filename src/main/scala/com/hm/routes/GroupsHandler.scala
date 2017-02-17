55cc4941d43dc3fed05d2c8aaddd31787ab7ca1c
vishnu667 <vishnu667@gmail.com>
2017-02-17 17:38:30 +0530

    added multiple traits

---
+++ src/main/scala/com/hm/routes/GroupsHandler.scala
@@ -0,0 +1,10 @@
+package com.hm.routes
+
+import spray.routing.HttpService
+
+/**
+  * Created by vishnu on 2/17/17.
+  */
+trait GroupsHandler extends HttpService{
+
+}
---
+++ src/main/scala/com/hm/routes/TodoHandler.scala
@@ -0,0 +1,10 @@
+package com.hm.routes
+
+import spray.routing.HttpService
+
+/**
+  * Created by vishnu on 2/17/17.
+  */
+trait TodoHandler extends HttpService{
+
+}
