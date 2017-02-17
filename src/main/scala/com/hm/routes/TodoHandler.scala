1c6ab4c1f311348d8d7cb4c5316a04b44c93de0a
pujacj <pujacj@gmail.com>
2017-02-17 18:26:08 +0530

    update todo

--- src/main/scala/com/hm/routes/TodoHandler.scala
+++ src/main/scala/com/hm/routes/TodoHandler.scala
@@ -1,10 +1,39 @@
 package com.hm.routes
 
+import com.hm.connector.Mysqlclient
 import spray.routing.HttpService
+import spray.json._
 
 /**
   * Created by vishnu on 2/17/17.
   */
-trait TodoHandler extends HttpService{
+trait TodoHandler extends HttpService {
+
+
+  /**
+    * Created by pooja on 17/2/17.
+    */
+  def updateTodo() = post {
+    entity(as[String]) {
+      body => {
+        val json = body.parseJson.asJsObject
+        val user_id = json.getFields("user_id").head.asInstanceOf[JsString].value
+        val message = json.getFields("message").head.asInstanceOf[JsString].value
+        if (!updateTodoApi(user_id.toInt, message)) {
+          complete("update successful")
+        }
+        else {
+          complete("update failed")
+        }
+      }
+    }
+
+  }
+
+
+  def updateTodoApi(user_id: Int, message: String) = {
+    val rs = Mysqlclient.executeQuery("update todo set message='" + message + "' where user_id='" + user_id + "'")
+    rs
+  }
 
 }
